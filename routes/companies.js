429ec9ae6fe87d9fba4699311de499e716e699ab
Hannah Lee <hannahsylee@gmail.com>
2022-03-02 21:21:03 -0800

    Further study

--- routes/companies.js
+++ routes/companies.js
@@ -1,6 +1,7 @@
 /** Routes for users of pg-intro-demo. */
 
 const express = require("express");
+const slugify = require("slugify");
 const ExpressError = require("../expressError")
 const router = express.Router();
 const db = require("../db");
@@ -50,13 +51,51 @@ router.get('/:code', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
     try {
-        const { code, name, description } = req.body;
+        let { name, description } = req.body;
+        let code = slugify(name, {lower: true});
         const results = await db.query('INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING code, name, description', [code, name, description]);
         return res.status(201).json({ company: results.rows[0] })
     } catch (e) {
         return next(e)
     }
 })
+
+router.put("/:id", async function (req, res, next){
+    try {
+        let { amt, paid } = req.body;
+        let id = req.params.id;
+        let paidDate = null;
+
+        const currResult = await db.query(
+            `SELECT paid
+            FROM invoices
+            WHERE id = $1`,
+            [id]);
+        if (currResult.rows.length === 0){
+            throw new ExpressError(`No such invoice: ${id}`, 404);
+        }
+        const currPaidDate = currResult.rows[0].paid_date;
+
+        if (!currPaidDate && paid) {
+            paidDate = new Date();
+        } else if (!paid) {
+            paidDate = null
+        } else {
+            paidDate = currPaidDate;
+        }
+
+        const result = await db.query(
+            `UPDATE invoices
+            SET amt$1, paid=$2, paid_date=$3
+            WHERE id=$4
+            RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+            [amt, paid, paidDate, id]);
+        return res.json({"invoice": result.rows[0]});
+    }
+    catch(err){
+        return next(err);
+    }
+});
 
 router.put("/:code", async function (req, res, next) {
     try {
